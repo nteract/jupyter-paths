6f6d166ca234ca236a4cc1a1aa78e6f51b0b4d4a
Kyle Kelley <rgbkrk@gmail.com>
2015-06-08 00:47:57 -0500

    Copy Pasta'ed a License statement

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * BSD 3-Clause License
  *
- * Copyright (c) 2015, Nicolas Riesco and others as credited in the AUTHORS file
+ * Copyright (c) 2015, Kyle Kelley and others as credited in the AUTHORS file
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
