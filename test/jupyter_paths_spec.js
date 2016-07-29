5d506074e4b3f1bcfe3166d0890a409eaca8fd09
Min RK <benjaminrk@gmail.com>
2016-07-29 17:18:41 +0200

    case-insensitive comparisons in tests
    seems necessary on Windows

--- test/jupyter_paths_spec.js
+++ test/jupyter_paths_spec.js
@@ -9,10 +9,15 @@ const execSync = require('child_process').execSync
 
 const actual = JSON.parse(execSync('jupyter --paths --json'))
 
+// case-insensitive comparisons
+actual.data = actual.data.map(path => { return path.toLowerCase(); });
+actual.config = actual.config.map(path => { return path.toLowerCase(); });
+
 describe('dataDirs', () => {
   it('returns a promise that resolves to a list of directories that exist', () => {
     return jp.dataDirs({withSysPrefix: true})
              .then((dirs) => {
+               dirs = dirs.map(dir => { return dir.toLowerCase() });
                expect(dirs).to.be.an('Array')
                dirs.forEach(el => {
                  expect(el).to.be.a('String')
@@ -22,6 +27,7 @@ describe('dataDirs', () => {
   })
   it('returns immediately with a guess when withSysPrefix is false', () => {
     var dirs = jp.dataDirs({withSysPrefix: false});
+    dirs = dirs.map(dir => { return dir.toLowerCase() });
     expect(dirs).to.be.an('Array');
     dirs.forEach(el => {
       expect(el).to.be.a('String')
@@ -40,6 +46,7 @@ describe('configDirs', () => {
   it('returns a promise that resolves to a list of directories that exist', () => {
     return jp.configDirs({withSysPrefix: true})
              .then((dirs) => {
+               dirs = dirs.map(dir => { return dir.toLowerCase() });
                expect(dirs).to.be.an('Array')
                dirs.forEach(el => {
                  expect(el).to.be.a('String')
@@ -49,6 +56,7 @@ describe('configDirs', () => {
   })
   it('returns immediately with a guess when withSysPrefix is false', () => {
     var dirs = jp.configDirs({withSysPrefix: false});
+    dirs = dirs.map(dir => { return dir.toLowerCase() });
     expect(dirs).to.be.an('Array');
     dirs.forEach(el => {
       expect(el).to.be.a('String')
